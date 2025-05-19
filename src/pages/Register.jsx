cde8608b686d35aa24222e0cb3572ceee035ba8d
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-05-19 07:39:21 +0200

    Feat: registerPage conectado al service

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+import { registerUser } from "../services/auth-service";
+
 export default function Register() {
+  const [phone, setPhone] = useState('')
+  const [name, setName] = useState('')
+  const [avatar, setAvatar] = useState(null)
+  const [error, setError] = useState(null)
+
+  const habdleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('phone', phone)
+    formData.append('name', name)
+
+    if (avatar) formData.append('avatar', avatar)
+
+    try {
+      await registerUser(formData)
+      alert('Usuario registrado con éxito')
+    } catch (err) {
+      setError(err.message)      
+    }
+  }
+
   return (
-    <div>
-      <h1>Registro</h1>
-      <form>
-        <input type="text" placeholder="Número de teléfono" required />
-        <input type="text" placeholder="Nombre (opcional)" />
-        <input type="file" />
-        <button type="submit">Registrarse</button>
-      </form>
-    </div>
-  );
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Teléfono" value={phone} onChange={(e) => setPhone(e.target.value)} />
+      <input type="text" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="file" onChange={(e) => setAvatar(e.target.files[0])} />
+      <button type="submit">Registrarse</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
+  )
+
 }
