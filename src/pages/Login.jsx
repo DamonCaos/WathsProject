d76bc9b88b310af8107ff9ef9c6d39750b217d00
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-05-19 11:35:48 +0200

    fix: añadido loader

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { loginUser } from "../services/auth-service.js";
-import { useAuth } from "../context/AuthContexrt.jsx";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { loginUser } from "../services/auth-service.js"
+import { useAuth } from "../context/AuthContexrt.jsx"
 
 export default function Login() {
   const [identifier, setIdentifier] = useState("")
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState("")
   const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
   const { login } = useAuth()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
+    setError(null)
     try {
       const data = await loginUser({ identifier, password })
       login(data.user, data.token)
-      navigate('/chats')
+      navigate("/chats")
     } catch (err) {
       setError(err.message)
-      
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -29,15 +33,19 @@ export default function Login() {
         placeholder="Teléfono o email"
         value={identifier}
         onChange={(e) => setIdentifier(e.target.value)}
+        disabled={loading}
       />
       <input
         type="password"
         placeholder="Contraseña"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        disabled={loading}
       />
-      <button type="submit">Iniciar sesión</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button type="submit" disabled={loading}>
+        {loading ? "Cargando..." : "Iniciar sesión"}
+      </button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </form>
   )
 }
